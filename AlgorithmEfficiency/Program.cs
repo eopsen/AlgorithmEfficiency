02c81cfed892f8c17c6e7de5b42ed89e3ea94953
maciej <mb@4sys.pl>
2022-11-09 15:55:40 +0100

    added test cases

--- AlgorithmEfficiency/Program.cs
+++ AlgorithmEfficiency/Program.cs
@@ -7,21 +7,22 @@ public class Test
     {
         var caseIdx = 1;
         var arrayTypes = new string[] {"Random", "Sorted", "Reversed", "AlmostSorted", "FewUnique"};
-        var testCases = new Dictionary<string, int>()
+
+        var testCases = new List<TestCase>()
         {
-            {"Próba mała", 10},
-            {"Próba średnia", 1000},
-            {"Próba duża", 100000}
+            new TestCase("Próba mała", 10, 1),
+            new TestCase("Próba średnia", 1000, 50),
+            new TestCase("Próba duża", 100000, 500)
         };
         
         foreach (var testCase in testCases)
         {
             foreach (var arrayType in arrayTypes)
             {
-                var arrToSort = GetArrayToSort(testCase.Value, arrayType);
+                var arrToSort = GetArrayToSort(testCase.Size, arrayType, testCase.PairsToSwap);
                 
                 Console.WriteLine();
-                Console.WriteLine($"Test {caseIdx}: {testCase.Key} ({testCase.Value}), {arrayType}");
+                Console.WriteLine($"Test {caseIdx}: {testCase.Name} ({testCase.Size}), {arrayType}");
                 
                 CreateReport("InsertionSort", arrToSort, new InsertionSort());
                 CreateReport("MergeSort", arrToSort, new MergeSort());
@@ -30,10 +31,9 @@ public class Test
                 caseIdx++;
             }
         }
-
     }
 
-    private static int[] GetArrayToSort(int size, string arrayType)
+    private static int[] GetArrayToSort(int size, string arrayType, int pairsToReplace)
     {
         switch (arrayType)
         {
@@ -44,21 +44,12 @@ public class Test
             case "Reversed":
                 return Generators.GenerateReversed(size, 0, size);
             case "AlmostSorted":
-                return Generators.GenerateAlmostSorted(size, size / 10);
+                return Generators.GenerateAlmostSorted(size, pairsToReplace);
             case "FewUnique":
-                return Generators.GenerateRandom(size, 0, size / 5);
+                return Generators.GenerateRandom(size, 0, pairsToReplace);
         }
 
         return new int[] { };
-    }
-
-    static void Print(int[] arr)
-    {
-        int n = arr.Length;
-        for (int i = 0; i < n; ++i)
-            Console.Write(arr[i] + " ");
- 
-        Console.Write("\n");
     }
 
     static void CreateReport(string algorithmName, int[] arr, ISortingAlgorithm sortingAlgorithm)
@@ -81,6 +72,19 @@ public class Test
     }
 }
 
+public class TestCase
+{
+    public string Name { get; set; }
+    public int Size { get; set; }
+    public int PairsToSwap { get; set; }
+
+    public TestCase(string _name, int _size, int _pairs)
+    {
+        Name = _name;
+        Size = _size;
+        PairsToSwap = _pairs;
+    }
+}
 public static class Extend
 {
     public static double StandardDeviation(this IEnumerable<long> values)
